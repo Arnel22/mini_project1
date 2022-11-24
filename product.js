1548786f75b0c3fcb87f0987220cf4f59e76ab8e
Arnel22 <118947801+Arnel22@users.noreply.github.com>
2022-11-24 13:14:55 +0800

    Add files via upload

---
+++ product.js
@@ -0,0 +1,211 @@
+$(document).ready(function(){
+
+    $('.thumb a').click(function(e){
+
+        e.preventDefault();
+        $('.imgBox img').attr("src", $(this).attr("href"))
+    })
+
+    $('#image1').click(function(e){
+
+        e.preventDefault();
+        $('#color').text("Gray")
+    })
+
+    $('#image2').click(function(e){
+
+        e.preventDefault();
+        $('#color').text("Dark Green")
+    })
+
+    $('#image3').click(function(e){
+
+        e.preventDefault();
+        $('#color').text("Black/Red")
+    })
+
+    // $('.thumb').click(function(){
+
+    //     $('#color')
+    // })
+
+})
+
+$(document).ready(function(){
+
+    $('.thumb a').click(function(e){
+
+        e.preventDefault();
+        $('.imgBox1 img').attr("src", $(this).attr("href"))
+    })
+
+    $('#imahe1').click(function(e){
+
+        e.preventDefault();
+        $('#color').text("GRAY")
+    })
+
+    $('#imahe2').click(function(e){
+
+        e.preventDefault();
+        $('#color').text("BLACK")
+    })
+
+    $('#imahe3').click(function(e){
+
+        e.preventDefault();
+        $('#color').text("BLUE/BLACK")
+    })
+
+    $('#imahe4').click(function(e){
+
+        e.preventDefault();
+        $('#color').text("WHITE/BLACK")
+    })
+
+    // $('.thumb').click(function(){
+
+    //     $('#color')
+    // })
+
+})
+
+// Jquery for honda wave
+
+$(document).ready(function(){
+
+    $('.thumb a').click(function(e){
+
+        e.preventDefault();
+        $('.imgBox2 img').attr("src", $(this).attr("href"))
+    })
+
+    $('#wave1').click(function(e){
+
+        e.preventDefault();
+        $('#color2').text("GRAY")
+    })
+
+    $('#wave2').click(function(e){
+
+        e.preventDefault();
+        $('#color2').text("RED/BLACK")
+    })
+
+    $('#wave3').click(function(e){
+
+        e.preventDefault();
+        $('#color2').text("GREEN/BLACK")
+    })
+
+    $('#wave4').click(function(e){
+
+        e.preventDefault();
+        $('#color2').text("BLACK")
+    })
+
+    // $('.thumb').click(function(){
+
+    //     $('#color')
+    // })
+
+})
+
+// Jquery for honda adv
+
+$(document).ready(function(){
+
+    $('.thumb a').click(function(e){
+
+        e.preventDefault();
+        $('.imgBox3 img').attr("src", $(this).attr("href"))
+    })
+
+    $('#adv1').click(function(e){
+
+        e.preventDefault();
+        $('#color3').text("RED/BLACK")
+    })
+
+    $('#adv2').click(function(e){
+
+        e.preventDefault();
+        $('#color3').text("WHITE/BLACK")
+    })
+
+    $('#adv3').click(function(e){
+
+        e.preventDefault();
+        $('#color3').text("GRAY/BLACK")
+    })
+
+
+})
+
+//Jquery for honda click
+
+$(document).ready(function(){
+
+    $('.thumb a').click(function(e){
+
+        e.preventDefault();
+        $('.imgBox4 img').attr("src", $(this).attr("href"))
+    })
+
+    $('#click1').click(function(e){
+
+        e.preventDefault();
+        $('#color4').text("BLACK")
+    })
+
+    $('#click2').click(function(e){
+
+        e.preventDefault();
+        $('#color4').text("GRAY")
+    })
+
+    $('#click3').click(function(e){
+
+        e.preventDefault();
+        $('#color4').text("GOLD")
+    })
+
+    $('#click4').click(function(e){
+
+        e.preventDefault();
+        $('#color4').text("RED/BLACK")
+    })
+})
+
+    //Jquery for honda pcx
+
+$(document).ready(function(){
+
+    $('.thumb a').click(function(e){
+
+        e.preventDefault();
+        $('.imgBox5 img').attr("src", $(this).attr("href"))
+    })
+
+    $('#pcx1').click(function(e){
+
+        e.preventDefault();
+        $('#color5').text("WHITE")
+    })
+
+    $('#pcx2').click(function(e){
+
+        e.preventDefault();
+        $('#color5').text("BROWN")
+    })
+
+    $('#pcx3').click(function(e){
+
+        e.preventDefault();
+        $('#color5').text("BLACK")
+    })
+
+
+
+})
+
